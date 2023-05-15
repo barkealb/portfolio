f37827ec4fad2bf7eeef074b97a62acaa3fc8a3c
Barkeal Beyene Gutema <barkealbeyene@gmail.com>
2023-05-15 13:02:04 -0400

    edit portfolio

--- barkfolio/src/index.js
+++ barkfolio/src/index.js
@@ -1,6 +1,9 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import { getMaxListeners } from "process";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+
